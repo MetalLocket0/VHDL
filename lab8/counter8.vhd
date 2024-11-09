5052cdcb171b9d7f471d4d927e0abdea94c6198c
MetalLocket0 <74558614+MetalLocket0@users.noreply.github.com>
2024-11-08 23:35:55 -0500

    modified:   lab8/counter8.vhd

--- lab8/counter8.vhd
+++ lab8/counter8.vhd
@@ -13,7 +13,7 @@ architecture counting of counter8 is
   signal int_count : UNSIGNED(9 downto 0);
   constant MAX_VAL : UNSIGNED(9 downto 0) := "1111100111";
 begin
-  process (clk, reset_n, set_n,enable, int_count)
+  process (clk, reset_n, set_n, enable, int_count)
   begin
     if (clk'event and clk = '1') then
       if (reset_n = '0') then
@@ -21,30 +21,14 @@ begin
       elsif (int_count = MAX_VAL) then
         int_count <= (others => '0');
       end if;
+      if (enable = '1') then
+        int_count <= int_count + 1;
+        if (set_n = '0') then
+          int_count <= unsigned(time_in);
+        end if;
+      end if;
     end if;
-end process;
-process (clk, reset_n, set_n,enable, int_count)
-begin
-  if (clk'event and clk = '1') then
-    if (reset_n = '0') then
-      int_count <= (others => '0');
-	 elsif (int_count = MAX_VAL) then 
-	  int_count <= (others => '0');
-	  end if;
-	  
-	 if (enable = '1') then
-      int_count <= int_count + 1;
-		
-		if (set_n ='1') then 
-		int_count <= unsigned(time_in);
-		end if;
-		
-		else 
-    
-	 end if;
-   
-	end if;
-  
+
   end process;
   count_out <= std_logic_vector(int_count);
 end counting;
