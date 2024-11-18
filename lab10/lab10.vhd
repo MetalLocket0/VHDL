366df2ff59bc6840a259f6f235e498a2e9bcbc74
MetalLocket0 <74558614+MetalLocket0@users.noreply.github.com>
2024-11-18 16:53:33 -0500

    modified:   lab10/lab10.vhd

--- lab10/lab10.vhd
+++ lab10/lab10.vhd
@@ -7,17 +7,17 @@ entity lab10 is
     reset                                                 : in std_logic;
     dime_in, nickel_in, quarter_in, coin_return, dispense : in std_logic;
     change_back, red_bull                                 : out std_logic;
-    HEX0, HEX1                                            : out std_logic_vector(6 downto 0);
+    HEX0, HEX1                                            : out std_logic_vector(6 downto 0) -- max 99 cents visually
   );
 end entity;
 
 architecture technique of lab10 is
   type state_type is (wait1, dime, nickel, quarter, enough, excess, vend, change);
   signal state, next_state : state_type;
-  signal money             : std_logic_vector(6 downto 0); -- max 127 cents 
+  signal money             : unsigned(6 downto 0); -- max 127 cents 
 begin
 
-  sync : process (clk, reset_n)
+  sync : process (clk, reset)
   begin
     if (reset = '0') then
       state <= wait1;
@@ -26,7 +26,7 @@ begin
     end if;
   end process;
 
-  comb : process (state,)
+  comb : process (state, quarter_in, dime_in, nickel_in, money, coin_return, dispense)
   begin
     case(state) is
       when wait1 =>
@@ -71,7 +71,7 @@ begin
         next_state <= enough;
       elsif (dispense = '1') then
         next_state <= vend;
-      elsif (dime_in or nickel_in or quarter_in = '1') then
+      elsif (dime_in = '1' or nickel_in = '1' or quarter_in = '1') then
         next_state <= excess;
       elsif (coin_return = '1') then
         next_state <= change;
@@ -80,7 +80,7 @@ begin
       when excess =>
       if (state = excess) then
         next_state <= enough;
-      elsif (dime_in or nickel_in or _quarter in = '1') then
+      elsif (dime_in = '1' or nickel_in = '1' or quarter_in = '1') then
         next_state <= excess;
       elsif (dispense = '1') then
         next_state <= vend;
@@ -100,9 +100,42 @@ begin
     end case;
   end process;
 
-  total : process (clk, state)
+  total : process (clk, reset, state)
   begin
 
+    if (reset = '0') then
+      money <= "0000000";
+    elsif (rising_edge(clk)) then
+      case(state) is
+
+        when wait1 =>
+        money <= money;
+
+        when dime =>
+        money <= (money + "0001010"); -- 10
+
+        when nickel =>
+        money <= (money + "0000101"); -- 5
+
+        when quarter =>
+        money <= (money + "0001001"); -- 25
+
+        when enough =>
+        money <= money;
+
+        when excess =>
+        money <= money;
+
+        when vend =>
+        money <= (money - "1001011"); --75
+
+        when change =>
+        money <= "0000000";
+
+        when others =>
+        money <= money;
+      end case;
+    end if;
   end process;
 
 end architecture;
